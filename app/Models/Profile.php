8c5ddc6069cc7af750413bd6b3f0dd99fbd53475
Account  Management <accountmanagement@adpia.vn>
2025-02-18 14:21:44 +0700

    update

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -11,4 +11,18 @@ class Profile extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+
+            $lastRecord = static::latest('affiliate_id')->first();
+
+            $number = $lastRecord ? ((int) substr($lastRecord->affiliate_id, 2)) + 1 : 1;
+
+            $model->affiliate_id = 'TK' . str_pad($number, 4, '0', STR_PAD_LEFT);
+        });
+    }
 }
