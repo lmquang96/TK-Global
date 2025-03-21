1bc1ee1050f837ff7f8b6479acbb80af8dfbac5c
Account  Management <accountmanagement@adpia.vn>
2025-03-21 10:13:03 +0700

    update

---
+++ app/Http/Controllers/Postback.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\CampaignPostback;
+
+class Postback extends Controller
+{
+    public function involve(Request $request) {
+        $data = $request->all();
+
+        CampaignPostback::create([
+            'source' => 'involve',
+            'data' => json_encode($data),
+            'campaign_id' => self::getCampaignId($data['offer_id'])
+        ]);
+
+        return response(['status' => 'success'], 200);
+    }
+
+    public function getCampaignId($adsId) {
+        $campaignId = '';
+
+        switch ($adsId) {
+            case 'value':
+                # code...
+                break;
+            
+            default:
+                $campaignId = 3;
+                break;
+        }
+        return $campaignId;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Upload;
+use App\Http\Controllers\Postback;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -13,3 +14,6 @@ Route::prefix('uploadOrder')->group(function () {
 
   Route::post('/storeAds', [Upload::class, 'storeAds']);
 });
+
+
+Route::get('/involve/postback', [Postback::class, 'involve']);
