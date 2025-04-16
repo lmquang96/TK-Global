7c89594de79f72d717fb0588b0dff8c52f77c596
Account  Management <accountmanagement@adpia.vn>
2025-04-16 15:29:30 +0700

    update

---
+++ app/Console/Kernel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+  /**
+   * Đăng ký các lệnh Artisan của ứng dụng.
+   *
+   * @return void
+   */
+  protected $commands = [
+    // Đăng ký các lệnh của bạn ở đây
+    Commands\ConversionsScan::class, // Ví dụ thêm command của bạn vào đây
+  ];
+
+  /**
+   * Định nghĩa các công việc lên lịch cho ứng dụng.
+   *
+   * @param \Illuminate\Console\Scheduling\Schedule $schedule
+   * @return void
+   */
+  protected function schedule(Schedule $schedule)
+  {
+    // Đặt lịch cho các công việc của bạn ở đây
+    $schedule->command('app:conversions-scan')->everyTenMinutes();
+  }
+
+  /**
+   * Đăng ký các dịch vụ Artisan.
+   *
+   * @return void
+   */
+  protected function commands()
+  {
+    $this->load(__DIR__ . '/Commands');
+
+    require base_path('routes/console.php');
+  }
+}
