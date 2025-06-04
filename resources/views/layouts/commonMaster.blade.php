ab7fb5cad48162187d564d2538c20f5294e918b7
Account  Management <accountmanagement@adpia.vn>
2025-06-04 09:30:09 +0700

    update

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -27,6 +27,10 @@
 
   <!-- Include Scripts for customizer, helper, analytics, config -->
   @include('layouts/sections/scriptsIncludes')
+
+  <script>
+    (function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A6277427-bd5b-473d-a7d5-efafc60517721.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');
+  </script>
 </head>
 
 <body>
