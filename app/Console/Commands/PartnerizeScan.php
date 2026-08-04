1d92d91e23fdcaf472ab7ecb7cdaa814d1929a45
Account  Management <accountmanagement@adpia.vn>
2026-08-04 14:01:52 +0700

    update

--- app/Console/Commands/PartnerizeScan.php
+++ app/Console/Commands/PartnerizeScan.php
@@ -8,33 +8,33 @@ use Carbon\Carbon;
 
 class PartnerizeScan extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:partnerize-scan {start_date?} {end_date?}';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'app:partnerize-scan {start_date?} {end_date?}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Command description';
 
-    /**
-     * Execute the console command.
-     */
-    public function handle(ScanPartnerizeService $scanPartnerizeService)
-    {
-      $startDate = $this->argument('start_date');
-      $endDate = $this->argument('end_date');
+  /**
+   * Execute the console command.
+   */
+  public function handle(ScanPartnerizeService $scanPartnerizeService)
+  {
+    $startDate = $this->argument('start_date');
+    $endDate = $this->argument('end_date');
 
-      $startDate = Carbon::createFromFormat('Ymd', $startDate);
-      $endDate = Carbon::createFromFormat('Ymd', $endDate);
+    $startDate = Carbon::createFromFormat('Ymd', $startDate);
+    $endDate = Carbon::createFromFormat('Ymd', $endDate);
 
-      $scanPartnerizeService->scan($startDate->format('Y-m-d'), $endDate->format('Y-m-d'));
+    $scanPartnerizeService->scan($startDate->format('Y-m-d'), $endDate->format('Y-m-d'));
 
-      return 0;
-    }
+    return 0;
+  }
 }
--- app/Services/Campaign/CampaignService.php
+++ app/Services/Campaign/CampaignService.php
@@ -18,7 +18,7 @@ class CampaignService
 
   public function getAll($request)
   {
-    return Cache::remember('campaigns_all', 600, function () use ($request) {
+    return Cache::remember('campaigns_all_' . $request->keyword . '_' . $request->category . '_' . $request->cpType, 300, function () use ($request) {
       return Campaign::statusActive()
         ->when($request->keyword, function ($q, $keyword) {
           $q->where('name', 'like', '%' . $keyword . '%');
